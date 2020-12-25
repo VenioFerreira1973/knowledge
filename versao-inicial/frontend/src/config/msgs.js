1939208f7f72f243d39f41ca40f1fe36ce2bed9c
VENIO LUIZ DE PAULA FERREIRA <venioferreira@yahoo.com.br>
2020-12-25 13:01:35 -0300

    msgs icons

--- versao-inicial/frontend/src/config/msgs.js
+++ versao-inicial/frontend/src/config/msgs.js
@@ -9,11 +9,11 @@ Vue.use(Toasted, {
 Vue.toasted.register(
   'defaultSuccess',
   payload => !payload.msg ? 'Operação realizada com sucesso!' : payload.msg,
-  { type: 'success' }
+  { type: 'success', icon: 'check' }
 )
  
 Vue.toasted.register(
   'defaultError',
   payload => !payload.msg ? 'Oops... Erro inesperado.' : payload.msg,
-  { type: 'error' }
+  { type: 'error', icon: 'times' }
 )
